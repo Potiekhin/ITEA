c9329d566c04ba0bdbdc69e745453f3e3abf8435
Oleh <49687041+Potiekhin@users.noreply.github.com>
2019-09-18 17:24:17 +0300

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double checkSumm = 337.77;
+            /*
+            double discount = 0;
+            
+            if (checkSumm <= 300)
+            {
+                
+                Console.WriteLine($"Сумма до сплати - {checkSumm - discount}\nВаша знижка - {discount}");
+            }
+            else if (checkSumm > 300 && checkSumm <= 400)
+            {
+                discount = checkSumm * 0.02;
+                Console.WriteLine($"Сумма до сплати - {checkSumm - discount}\nВаша знижка - {discount}");
+            }
+            else if (checkSumm > 400 && checkSumm < 500)
+            {
+                discount = checkSumm * 0.03;
+                Console.WriteLine($"Сумма до сплати - {checkSumm - discount}\nВаша знижка - {discount}");
+            }
+            else if (checkSumm >= 500)
+            {
+                discount = checkSumm * 0.05;
+                Console.WriteLine($"Сумма до сплати - {checkSumm - discount}\nВаша знижка - {discount}");
+            }
+            Console.ReadKey();
+            */
+
+            double discount =
+                checkSumm <= 300 ? 0 : 
+                checkSumm > 300 && checkSumm <= 400 ? checkSumm * 0.02 : 
+                checkSumm > 400 && checkSumm < 500 ? checkSumm * 0.03 : 
+                checkSumm * 0.05;
+
+            discount = Math.Round(discount, 2);
+
+            Console.WriteLine($"Сумма до сплати - {checkSumm - discount}\nВаша знижка - {discount}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task1_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1._1
+{
+    class Task_2
+    {
+        static void Main(string[] args)
+        {
+            double Money = 2.22;
+            double Leffe = 80;
+            double Hoegaarden = 70;
+            double Stella_Artois = 60;
+            double Obolon = 7;
+
+            string answer =
+                Money >= Leffe ? $" Your choise Leffe and rest {Money - Leffe}" :
+                Money < Leffe && Money >= Hoegaarden ? $" Your choise Hoegaarden and rest {Money - Hoegaarden}" :
+                Money < Hoegaarden && Money >= Stella_Artois ? $" Your choise Hoegaarden and rest {Money - Stella_Artois}" :
+                Money < Stella_Artois && Money >= Obolon ? $" Your choise Hoegaarden and rest {Money - Obolon}" :
+                $"You have only {Money}, Surprise your liver! drink water!!!";
+            Console.WriteLine(answer);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
