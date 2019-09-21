f7f7021e47d42219006eb7515d448e555019f4a1
3.14ton <potiehin.o@gmail.com>
2019-09-21 18:09:39 +0300

    Task 2

---
+++ Task2_0.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._14ton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int enter = int.Parse(Console.ReadLine());
+            int LengNum = enter.ToString().Length;        //довжина числа 
+
+            int evenSumm = 0;
+            int thereDivineNumbers = 0;
+
+            for (int i = 0; i < LengNum; i++)
+            {
+                int Numbers = enter % 10;           // зворотній перебір
+                enter = enter / 10;                 // зворотній перебір
+
+                if (Numbers % 2 == 0)
+                {
+                    evenSumm += Numbers;
+                }
+
+                if (Numbers % 3 == 0)
+                {
+                    thereDivineNumbers++;
+                }
+
+            }
+
+            Console.WriteLine("Кількість символів у введеному числі : " + LengNum);
+            Console.WriteLine("Кількість символів у введеному числі : " + evenSumm);
+            Console.WriteLine("Кількість символів у введеному числі : " + thereDivineNumbers);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2_1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int randomNuber = r.Next(0, 100);
+
+            for (int i = 1; i < 6; i++)
+            {
+
+                Console.WriteLine($"try №{i} Enter the number between 0 and 100");
+                int enter = int.Parse(Console.ReadLine());
+                if (enter > randomNuber)
+                {
+                    Console.WriteLine("NO! Enter Smaller number");
+                }
+                else if (enter < randomNuber)
+                {
+                    Console.WriteLine("NO! Enter Biger number");
+                }
+                else if (enter == randomNuber)
+                {
+                    Console.WriteLine("YOU WIN!!!");
+                    break;
+                }
+                if (i == 5)
+                {
+                    Console.WriteLine("SORRY! YOU LOSE!");
+                }
+            }
+
+            Console.WriteLine(randomNuber);
+            Console.ReadKey();
+
+
+        }
+    }
+}
