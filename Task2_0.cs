85ebd6295b1e88b69e3557744e33106be47f1006
3.14ton <potiehin.o@gmail.com>
2019-09-23 19:32:34 +0300

    Enother solution

--- Task2_0.cs
+++ Task2_0.cs
@@ -10,17 +10,18 @@ namespace _3._14ton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number");
+            Console.WriteLine("Введіть ціле число");
             int enter = int.Parse(Console.ReadLine());
-            int LengNum = enter.ToString().Length;        //довжина числа 
+            int LengNum = enter.ToString().Length;
 
             int evenSumm = 0;
             int thereDivineNumbers = 0;
 
             for (int i = 0; i < LengNum; i++)
             {
-                int Numbers = enter % 10;           // зворотній перебір
-                enter = enter / 10;                 // зворотній перебір
+                int Numbers = enter / 10 * 10;
+                Numbers = enter - Numbers;
+                enter = enter / 10;
 
                 if (Numbers % 2 == 0)
                 {
@@ -35,8 +36,8 @@ namespace _3._14ton
             }
 
             Console.WriteLine("Кількість символів у введеному числі : " + LengNum);
-            Console.WriteLine("Кількість символів у введеному числі : " + evenSumm);
-            Console.WriteLine("Кількість символів у введеному числі : " + thereDivineNumbers);
+            Console.WriteLine("Сумма парних чисел : " + evenSumm);
+            Console.WriteLine("Кількість цифр кратних 3 : " + thereDivineNumbers);
             Console.ReadKey();
         }
     }
