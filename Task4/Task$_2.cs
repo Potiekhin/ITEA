ee703e306b20c8c4090d33ed67e7cd00d235bbff
3.14ton <potiehin.o@gmail.com>
2019-10-03 18:49:24 +0300

    task4_2

---
+++ Task4/Task$_2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Strings
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть прізвище ім'я та побатькові");
+            string input = Console.ReadLine();
+
+            int firstIndex = input.IndexOf(" ");
+            int secondIndex = input.IndexOf(" ", firstIndex + 1);
+
+            string secondName1 = input.Remove(input.LastIndexOf(" ") + 1);
+            string secondName = secondName1.Substring(secondName1.IndexOf(" ") + 1);
+            string firstName = input.Remove(input.IndexOf(" ") + 1);
+            string fatherName = input.Remove(0, secondIndex + 1);
+
+            string firstNameFW = firstName.Remove(1);
+            string secondNameFW = secondName.Remove(1);
+            string fatherNameFW = fatherName.Remove(1);
+            string lastWords = firstName.Substring(1);
+
+            Console.WriteLine(firstNameFW.ToUpper() + lastWords + " " + secondNameFW.ToUpper() + "." + fatherNameFW.ToUpper() + ".");
+            Console.ReadKey();
+        }
+    }
+}
